c0c8f41c6f9691cf3f356644057a9048938fdf76
AniiB <ani.b@tuta.io>
2022-06-18 02:31:16 +0530

    CRUD functionality added

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,85 @@
-console.log('Hey its working')
+console.log(`I.SEE.YOU.\n\t  |\\__/,|   ('\\\n \t_.|o o  |___ ) )\n\t-(((---(((--------`)
 
 const checkMarks = document.querySelectorAll('.fa-check')
 const undoMarks = document.querySelectorAll('.fa-rotate-left')
+const deleteMarks = document.querySelectorAll('.fa-xmark')
+const reset = document.querySelector('.reset')
 
 checkMarks.forEach(check => check.addEventListener('click', markAsDone))
 undoMarks.forEach(undo => undo.addEventListener('click', markUndone))
-async function markAsDone(event) {
-    let task = event.target.parentElement.previousElementSibling.outerText
+deleteMarks.forEach(del => del.addEventListener('click', deleteTask))
+reset.addEventListener('click', deleteAllTasks)
 
-    let response = await fetch('/markdone', {
-        method: 'put',
-        headers : { 'Content-Type' : 'application/json' },
-        body : JSON.stringify({task})
-    })
-    let data = await response.json()
-    console.log(data)
-    location.reload()
+async function markAsDone(event) {
+    try {
+        let task = event.target.parentElement.previousElementSibling.outerText
+
+        let response = await fetch('/markdone', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ task })
+        })
+        let data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch (error) {
+        console.error(error)
+    }
+
 
 }
 
 async function markUndone(event) {
-    let task = event.target.parentElement.previousElementSibling.outerText
+    try {
+        let task = event.target.parentElement.previousElementSibling.outerText
 
-    let response = await fetch('/markundone', {
-        method: 'put',
-        headers : { 'Content-Type' : 'application/json' },
-        body : JSON.stringify({task})
-    })
-    let data = await response.json()
-    console.log(data)
-    location.reload()
+        let response = await fetch('/markundone', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ task })
+        })
+        let data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch (error) {
+        console.error(error)
+    }
+
+
+}
+
+async function deleteTask(event) {
+    try {
+        let task = event.target.parentElement.previousElementSibling.outerText
+
+        let response = await fetch('/deletetask', {
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ task })
+        })
+        let data = await response.json()
+        console.log(data)
+        location.reload()
+
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+async function deleteAllTasks() {
+    try {
+        let response = await fetch('/deleteall', {
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({})
+        })
+        let data = await response.json()
+        console.log(data)
+        location.reload()
+
+    } catch (error) {
+        console.error(error)
+    }
 
 }
--- server.js
+++ server.js
@@ -3,41 +3,49 @@ const express = require('express'),
     app = express(),
     MongoClient = require('mongodb').MongoClient,
     dbName = 'tasks';
-
 let db;
+
+//use of .env files
 require('dotenv').config()
+
+//use .env file for connection string
 const dbConnectionString = process.env.DB_STRING
+
+// function call to connect to DB
 connectDB()
+
 
 //MiddleWares
 app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
-
 app.use(express.static('public'))
 app.use(express.json())
-
 
 
 //===============================================
 // ROUTES
 //===============================================
 
+// root request
 app.get('/', async (req,res) => {
     let tasks = await db.collection(dbName).find().toArray()
     res.render('index.ejs', {taskList: tasks})
 })
 
-
-
+//Create request to create a new task
 app.post('/addtask', async (req, res) => {
+    if(!req.body.task) return res.redirect('/')
+
+    let taskReq = req.body.task.trim()
     let response = await db.collection(dbName).insertOne({
-        task: req.body.task,
+        task: taskReq,
         done: false
     })
     console.log(response)
     res.redirect('/')
 })
 
+//Update request to mark task as complete
 app.put('/markdone', async (req,res) => {
     let response = await db.collection(dbName).updateOne( {
         task: req.body.task,
@@ -50,6 +58,7 @@ app.put('/markdone', async (req,res) => {
     res.json('Update Completed')
 })
 
+//Update request to mark task as incomplete
 app.put('/markundone', async (req,res) => {
     let response = await db.collection(dbName).updateOne( {
         task: req.body.task,
@@ -62,27 +71,24 @@ app.put('/markundone', async (req,res) => {
     res.json('Update Completed')
 })
 
+//Delete request to clear a single task
+app.delete('/deletetask', async (req,res) => {
+    console.log(req.body)
+    let response = await db.collection(dbName).deleteOne( {
+        task: req.body.task,
+    })
+    console.log(response)
+    res.json('Delete Successful')
+})
 
+//DELETE request to clear all tasks
+app.delete('/deleteall', async (req,res) => {
+    let response = await db.collection(dbName).deleteMany({})
+    console.log(response)
+    res.json('Clear successful')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Connect to MongoDB
 async function connectDB() {
     try {
         const client = await MongoClient.connect(dbConnectionString, { useUnifiedTopology: true })
@@ -95,8 +101,6 @@ async function connectDB() {
 
 
 }
-
-
 
 const PORT = 8000
 app.listen(process.env.PORT || PORT, () => {
